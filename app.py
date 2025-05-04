8f624071766cf8b7a3266062a0f5932e564529bb
Haruka Hirota <95326748+HARUKA2910@users.noreply.github.com>
2025-05-04 15:47:37 +0000

    変更

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import json
 
 app = Flask(__name__)
 
-# 店情報を読み込む関数
+# JSONデータを読み込む関数
 def load_restaurants():
     with open("restaurants.json", "r", encoding="utf-8") as file:
         return json.load(file)
 
 @app.route("/")
 def index():
+    # JSONデータを読み込む
     restaurants = load_restaurants()
+    # テンプレートにデータを渡す
     return render_template("index.html", restaurants=restaurants)
 
 if __name__ == "__main__":
